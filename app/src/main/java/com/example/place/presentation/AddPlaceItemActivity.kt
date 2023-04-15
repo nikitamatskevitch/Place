6b1b3baf8153fca98a883d10d09289f8a6a78d4b
Nick <dadadadada0405@gmail.com>
2023-04-15 23:40:56 +0300

    Finished activity_add_place_item.xml

---
+++ app/src/main/java/com/example/place/presentation/AddPlaceItemActivity.kt
@@ -0,0 +1,11 @@
+package com.example.place.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPlaceItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_place_item)
+    }
+}
